52eb733819528469310807bc09829e8c692cc14b
Suraj <suraj.oliver@gmail.com>
2021-07-14 17:09:43 +0530

    settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -123,6 +123,7 @@ print("***** BASE_DIR", BASE_DIR)
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 print("***** STATIC ROOT", STATIC_ROOT)
 
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
